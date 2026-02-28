8aa9d91f8ac0fda217955cf52c31ad6edd9ade2c
Nicolas Kane <nicolaskanestraw@gmail.com>
2026-02-27 21:11:40 -0300

    v4

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { useAuthStore } from './store/useAuthStore';
 import { AuthPage } from './features/auth/AuthPage';
@@ -44,7 +44,7 @@ function App() {
 
   return (
     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/auth" element={<AuthPage />} />
 
@@ -57,7 +57,7 @@ function App() {
 
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </GoogleOAuthProvider>
   );
 }
